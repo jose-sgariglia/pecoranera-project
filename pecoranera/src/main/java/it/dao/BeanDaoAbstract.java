2b29aa18dcf66f132d0b004c900a8a5d9ccce02e
jose-sgariglia <josesugariria@gmail.com>
2023-05-18 17:52:22 +0200

    Implementazione della doDelete nella Abstract class

--- pecoranera/src/main/java/it/dao/BeanDaoAbstract.java
+++ pecoranera/src/main/java/it/dao/BeanDaoAbstract.java
@@ -1,29 +1,52 @@
 package it.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.Collection;
 
 import javax.sql.DataSource;
 
-public abstract class BeanDaoAbstract<T> implements BeanDaoInterface<T>{
+public abstract class BeanDaoAbstract<T>{
 	protected DataSource ds = null;
+	protected String TABLE_NAME;
 
 	public BeanDaoAbstract(DataSource ds) {
 		this.ds = ds;
 	}
 
-	@Override
 	public abstract void doSave(T item) throws SQLException;
 
-	@Override
 	public abstract void doUpdate(T item) throws SQLException, UnsupportedOperationException;
 
-	@Override
-	public abstract boolean doDelete(int id) throws SQLException;
+	public boolean doDelete(int id) throws SQLException {
+		Connection connection = null;
+		PreparedStatement preparedStatement = null;
 
-	@Override
+		int result = 0;
+
+		String deleteSQL = "DELETE FROM " + this.TABLE_NAME + " WHERE id_" + this.TABLE_NAME + " = ?";
+
+		try {
+			connection = ds.getConnection();
+			preparedStatement = connection.prepareStatement(deleteSQL);
+			preparedStatement.setInt(1, id);
+
+			result = preparedStatement.executeUpdate();
+
+		} finally {
+			try {
+				if (preparedStatement != null)
+					preparedStatement.close();
+			} finally {
+				if (connection != null)
+					connection.close();
+			}
+		}
+		return (result != 0);
+	}
+
 	public abstract T doRetrieveByKey(int id) throws SQLException;
 
-	@Override
 	public abstract Collection<T> doRetrieveAll() throws SQLException;
 }
